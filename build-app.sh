9751b9e1f39c9659f9d049ba4f5c8b8c1d739812
Gabriel Meléndez Ponce <gabriel@Gabriels-MacBook-Pro.local>
2025-06-18 21:01:15 -0600

    removing cache

--- build-app.sh
+++ build-app.sh
@@ -1,17 +1,12 @@
-#!/bin/bash
-# Make sure this file has executable permissions, run `chmod +x build-app.sh`
-
-# Exit the script if any command fails
+#!/usr/bin/env bash
 set -e
 
-# Build assets using NPM
 npm run build
 
-# Clear cache
-php artisan optimize:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
 
-# Cache the various components of the Laravel application
 php artisan config:cache
-php artisan event:cache
 php artisan route:cache
 php artisan view:cache
