838dbf500a9ab4c11ef3417d12fd0ff73ce514b9
Gabriel Meléndez Ponce <gabriel@Gabriels-MacBook-Pro.local>
2025-06-18 21:53:49 -0600

    fix

--- build-app.sh
+++ build-app.sh
@@ -1,11 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Make sure this file has executable permissions, run `chmod +x build-app.sh`
+
+# Exit the script if any command fails
 set -e
 
-php artisan config:clear
-php artisan cache:clear
-
+# Build assets using NPM
 npm run build
 
+# Clear cache
+php artisan optimize:clear
+
+# Cache the various components of the Laravel application
 php artisan config:cache
+php artisan event:cache
 php artisan route:cache
 php artisan view:cache
