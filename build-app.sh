a7b7e209486bd016ff416f951a816744def474bb
Gabriel Meléndez Ponce <gabriel@Gabriels-MacBook-Pro.local>
2025-06-18 21:39:56 -0600

    fix build

--- build-app.sh
+++ build-app.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-npm run build
-
 php artisan config:clear
-php artisan route:clear
-php artisan view:clear
+php artisan cache:clear
+
+npm run build
 
 php artisan config:cache
 php artisan route:cache
